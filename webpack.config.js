37bf2f4de204893c5f4e1c09250eeb051b2a6438
Vitaliy Derda <derdavitaliy@gmail.com>
2022-03-24 19:45:11 +0200

    fix webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
   },
   output: {
     filename: "app.js",
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "build"),
     sourcePrefix: "",
   },
   amd: {
